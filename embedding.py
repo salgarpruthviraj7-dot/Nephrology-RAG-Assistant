3abeb4fa74b92ecf19e356af556d8d8fb5db8f7a
salgarpruthviraj7-dot <salgarpruthviraj7@gmail.com>
2026-06-26 17:28:52 +0530

    Delete embedding.py

--- embedding.py
+++
@@ -1,7 +0,0 @@
-from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_community.vectorstores import FAISS
-def embed(chunks):
-    emb = HuggingFaceEmbeddings(model_name = "sentence-transformers/all-MiniLM-L6-v2")
-    vectordatabase = FAISS.from_documents(chunks,emb)
-    vectordatabase.save_local("fiass_index_database")
-    return "Created a vector database named : faiss_index_database"
